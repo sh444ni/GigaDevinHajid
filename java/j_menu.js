12f9c4cac4fc47be7d7ffba46b792dffe36ebed4
DevinNuralim052 <2300018052@webmail.uad.ac.id>
2024-04-26 15:53:07 +0700

    Update sidebar

--- java/j_menu.js
+++ java/j_menu.js
@@ -1,7 +1,23 @@
-const toggle = document.querySelector('.toggle');
-const navbar = document.querySelector('.navbar');
+window.onload = function(){
+  const sidebar = document.querySelector(".sidebar");
+  const closeBtn = document.querySelector("#btn");
+  const searchBtn = document.querySelector(".bx-search")
 
-toggle.addEventListener('click', () => {
-  toggle.classList.toggle('active');
-  navbar.classList.toggle('open');
-});
+  closeBtn.addEventListener("click",function(){
+      sidebar.classList.toggle("open")
+      menuBtnChange()
+  })
+
+  searchBtn.addEventListener("click",function(){
+      sidebar.classList.toggle("open")
+      menuBtnChange()
+  })
+
+  function menuBtnChange(){
+      if(sidebar.classList.contains("open")){
+          closeBtn.classList.replace("bx-menu","bx-menu-alt-right")
+      }else{
+          closeBtn.classList.replace("bx-menu-alt-right","bx-menu")
+      }
+  }
+}
