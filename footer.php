719522f2b29ce08ee7d98535224fc1c934f3493c
B054 Giga Syakir <2300018054@webmail.uad.ac.id>
2024-06-14 00:03:34 +0700

    14/06/24

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer>
+      <div class="footerContainer">
+          <div class="socialIcons">
+              <a href="https://github.com/sh444ni/GigaDevinHajid"><i class='bx bxl-github'></i></a>
+              <a href="https://www.youtube.com/watch?v=jCzez_q8si0"><i class='bx bxl-youtube'></i></a>
+          </div>
+          <div class="footerNav">
+              <ul><li><a href="./register.html">Register</a></li>
+                  <li><a href="">Contact Us</a></li>
+              </ul>
+          </div>
+          
+      </div>
+      <div class="footerBottom">
+          <p>Golden State Warriors &copy;2024</p>
+      </div>
+  </footer>
---
+++ header.php
@@ -0,0 +1,80 @@
+<header class="header">
+        <a href="https://www.youtube.com/watch?v=Iq4Ur-7RIY4" class="logo">GSW</a>
+
+        <nav class="navbar">
+            <a href="./about.html">About</a>       
+            <a href="./login.html">Login</a>
+        </nav>
+
+        <div class="sidebar">
+            <div class="logo_details">
+              <i class='bx bx-basketball icon'></i>
+              <div class="logo_name">Hi Warriors!</div>
+              <i class="bx bx-menu" id="btn"></i>
+            </div>
+            <ul class="nav-list">
+              <li>
+                <a href="./index.html">
+                  <i class='bx bx-home'></i>
+                  <span class="link_name">Home</span>
+                </a>
+                <span class="tooltip">Home</span>
+              </li>
+              <hr>
+              <li>
+                <a href="./profile.html">
+                  <i class="bx bx-user"></i>
+                  <span class="link_name">Profile</span>
+                </a>
+                <span class="tooltip">Profile</span>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="bx bx-cog"></i>
+                  <span class="link_name">Settings Profile</span>
+                </a>
+                <span class="tooltip">Settings Profile</span>
+              </li>
+              <hr>
+              <li>
+                <a href="#">
+                  <i class="bx bx-cart-alt"></i>
+                  <span class="link_name">Order</span>
+                </a>
+                <span class="tooltip">Order</span>
+              </li>
+              <hr>
+              <li>
+                <a href="./champ.html">
+                  <i class='bx bxs-t-shirt'></i>
+                  <span class="link_name">'21-'22 Champions</span>
+                </a>
+                <span class="tooltip">'21-'22 Champions</span>
+              </li>
+              <li>
+                <a href="./jersey.html">
+                  <i class='bx bxs-t-shirt'></i>
+                  <span class="link_name">Jerseys</span>
+                </a>
+                <span class="tooltip">Jerseys</span>
+              </li>
+              <li>
+                <a href="by_player.html">
+                  <i class='bx bxs-t-shirt'></i>
+                  <span class="link_name">By Player</span>
+                </a>
+                <span class="tooltip">By Player</span>
+              </li>
+              <li class="profile">
+                <div class="profile_details">
+                  <img src="./img/devin.jpg" alt="profile image">
+                  <div class="profile_content">
+                    <div class="name">Anna Jhon</div>
+                    <div class="designation">Admin</div>
+                  </div>
+                </div>
+                <i class="bx bx-log-out" id="log_out"></i>
+              </li>
+            </ul>
+          </div>
+    </header>
